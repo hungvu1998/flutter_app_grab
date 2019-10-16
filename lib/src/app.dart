ae3f8c739fc0f46a0a516f962470f71b2cf6a5db
Vu Viet Hung <hungvv@rikkeisoft.com>
2019-10-16 17:33:27 +0700

    Initial commit

---
+++ lib/src/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_grab/src/resources/login_page.dart';
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginPage(),
+    );
+  }
+}
---
+++ lib/src/resources/register_page.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Color(0xff3277D8)),
+        elevation: 0,//bỏ shawdown
+      ),
+      body: Container(
+        constraints: BoxConstraints.expand(),
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 80,
+              ),
+              Image.asset('ic_car_red.png'),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0,40,0,6),
+                child: Text("Welcome new user!",style: TextStyle(
+                  fontSize: 22,color: Color(0xff333333),
+                ),),
+              ),
+              Text("Signup with iCar in simple steps",style: TextStyle(
+                fontSize: 16,color: Color(0xff606470),
+              ),),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0,145,0,20),
+                child: TextField(
+                  style: TextStyle(fontSize: 18,color: Colors.black),
+                  decoration: InputDecoration(
+                      labelText: "Name",
+                      prefixIcon: Container(
+                        width: 50,
+                        child: Image.asset("ic_user.png"),
+                      ),
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Color(0xffCED0D2),width: 1
+                          ),
+                          borderRadius: BorderRadius.all(
+                              Radius.circular(6))
+                      )
+                  ),
+                ),
+              ),
+              TextField(
+                style: TextStyle(fontSize: 18,color: Colors.black),
+                decoration: InputDecoration(
+                    labelText: "Phone number",
+                    prefixIcon: Container(
+                      width: 50,
+                      child: Image.asset("ic_phone.png"),
+                    ),
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color(0xffCED0D2),width: 1
+                        ),
+                        borderRadius: BorderRadius.all(
+                            Radius.circular(6))
+                    )
+                ),
+              ),
+              TextField(
+                style: TextStyle(fontSize: 18,color: Colors.black),
+                decoration: InputDecoration(
+                    labelText: "Email",
+                    prefixIcon: Container(
+                      width: 50,
+                      child: Image.asset("ic_mail.png"),
+                    ),
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color(0xffCED0D2),width: 1
+                        ),
+                        borderRadius: BorderRadius.all(
+                            Radius.circular(6))
+                    )
+                ),
+              ),
+              TextField(
+                style: TextStyle(fontSize: 18,color: Colors.black),
+                decoration: InputDecoration(
+                    labelText: "Password",
+                    prefixIcon: Container(
+                      width: 50,
+                      child: Image.asset("ic_lock.png"),
+                    ),
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color(0xffCED0D2),width: 1
+                        ),
+                        borderRadius: BorderRadius.all(
+                            Radius.circular(6))
+                    )
+                ),
+              ),
+              Padding (
+                padding: const EdgeInsets.fromLTRB(0,30,0,40),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 52,
+                  child: RaisedButton(
+                    onPressed: (){},
+                    child: Text("Log In",
+                      style: TextStyle(color: Colors.white,fontSize: 18),
+                    ),
+                    color: Color(0xff3277D8),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(6)),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0,0,0,40),
+                child: RichText(
+                  text: TextSpan(
+                      text: "Already a User? ",
+                      style: TextStyle(color: Color(0xff606470),fontSize: 16),
+                      children: <TextSpan>[
+                        TextSpan(
+                            text: "Login now",
+                            style: TextStyle(
+                                color: Color(0xff3277D8),fontSize: 16
+                            )
+                        )
+                      ]
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
